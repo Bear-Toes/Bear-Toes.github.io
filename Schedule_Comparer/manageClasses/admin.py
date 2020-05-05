737985bce901b4bcafc92d5f2595c872d8d4e89b
George <farwalkermaster@gmail.com>
2020-05-05 17:13:30 -0400

    Register more models to access from admin control panel, migrated models

--- Schedule_Comparer/manageClasses/admin.py
+++ Schedule_Comparer/manageClasses/admin.py
@@ -3,3 +3,6 @@ from .models import Student, Teacher, FSSClass, AfterSchoolActivity
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(FSSClass)
+admin.site.register(AfterSchoolActivity)
