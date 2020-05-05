06c5400f078cf6b749cf66af3535a6a2bb536adc
George <farwalkermaster@gmail.com>
2020-05-05 10:53:54 -0400

    Adding a page to view individual students

--- Schedule_Comparer/manageClasses/views.py
+++ Schedule_Comparer/manageClasses/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Student, Teacher, FSSClass, AfterSchoolActivity
 
 # Create your views here.
 def index(request):
     all_students = Student.objects.all()
-    context = {
-        'all_students': all_students,
-    }
-    return render(request, 'manageClasses/index.html', context)
+    return render(request, 'manageClasses/index.html', {'all_students': all_students})
 
 def detail(request, name):
-    print("I tried to render")
-    return HttpResponse("<h1>This is the page all about the student named " + str(name) + ".</h1>")
+    try:
+        stud = Student.objects.get(name=name)
+    except stud.DoesNotExist:
+        raise Http404("Student Does Not Exist")
+    return render(request, 'manageClasses/detail.html', {'student': stud})
